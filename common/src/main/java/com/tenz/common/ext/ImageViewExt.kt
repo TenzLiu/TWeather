b8ebafc3745a6367e1ef169b8bfe9b17e5239ba7
TenzLiu <TenzLiuIU1314>
2021-12-07 18:31:39 +0800

    coil处理

---
+++ common/src/main/java/com/tenz/common/ext/ImageViewExt.kt
@@ -0,0 +1,10 @@
+package com.tenz.common.ext
+
+import android.widget.ImageView
+
+fun ImageView.loadImage(url: String){
+//    this.load(url) {
+//        crossfade(true)
+//        placeholder(R.mipmap.logo)
+//        transformations(CircleCropTransformation())
+}
