003711f564149bbba3396da6e9d5521351dccb21
TenzLiu <TenzLiuIU1314>
2021-12-08 18:13:06 +0800

    彩云token

--- common/src/main/java/com/tenz/common/app/Constants.kt
+++ common/src/main/java/com/tenz/common/app/Constants.kt
@@ -7,6 +7,9 @@ class Constants {
 
     companion object {
 
+        //彩云api-token(https://dashboard.caiyunapp.com/user/user/info/)
+        const val CAIYUN_TOKEN = "9Be2VWWIPrr7xDgJ"
+
         //路由地址
         const val PATH_MAIN = "/app/MainActivity"
         const val PATH_LOGIN = "/login/LoginActivity"
